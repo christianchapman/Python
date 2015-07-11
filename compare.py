8df171bd16d369396e44e6ee018aa394d1741bd6
christianchapman <christian@networkcloudconsulting.com>
2015-07-11 14:43:22 +0300

    compare

---
+++ compare.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+import sys
+import os
+import pdb
+
+chris = "fuck shit piss"
+x = "fu"
+cmd = ""
+test = sys.argv[1]
+f = open (test,'r')
+odd = ""
+for cnt,chris in enumerate(f.readlines()):
+#    print " chris is = %s, count is %s\n" %(chris,cnt)
+     buffer = chris.split("\t");
+     ip = buffer[0]
+     ip = ip.strip()
+     name = buffer[1]
+     name = name.strip()
+     if ("roo.iq") not in name :
+         if ("ROO.IQ") not in name :
+            name = name + ".roo.iq"
+     cmd = "dig %s" %(name)
+  #   print "command is %s\n" %cmd
+     o = os.popen(cmd)
+     es = o.read();
+     buffer = es.split("\n");
+   #       #pdb.set_trace()
+     for line in buffer :
+           if "A" in line :
+               print "found A in %s\n" %line
+           if ( ("\tA\t" in line) & (";" not in line) ) :
+               print "%s -->  %s --> %s" %(name,ip,line)
+           if ( ("SOA" in line) & (";" not in line) ) :
+               print "%s --> %s --> %s" %(name,ip,line)
